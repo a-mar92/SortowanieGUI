6ba51d0f39cd1df57616da641296ddc9474949cc
a-mar92 <57841204+a-mar92@users.noreply.github.com>
2023-03-17 13:30:06 +0100

    Add files via upload

--- Sortowanie_UI/src/DrawFrame.java
+++ Sortowanie_UI/src/DrawFrame.java
@@ -1,42 +1,82 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
-class DrawFrame extends JFrame implements ActionListener {
-
-    private JButton bPrintInfo, bExit;
-
-    public DrawFrame(){
-        dzialanie();
+public class DrawFrame extends JFrame implements ActionListener {
+    ArrayList<Integer>numbers = new ArrayList<>();
+    JButton bExit, bSort;
+    JFrame myFrame;
+    JTextField imputNumbersTextArea;
+    JLabel numberInformation, sortedNumbers;
+    String inputedNumbersTextField;
+    String table [];
+    public String getInputedNumbersTextField() {
+        return inputedNumbersTextField;
     }
 
-    private void dzialanie() {
-        setTitle("Sortowanie");
-        setSize(450,200);
-        bPrintInfo = new JButton("Naciśnij");
-        setLayout(null);
-        bPrintInfo.setBounds(230,125,200,25);
-        add(bPrintInfo);
-        setVisible(true);
+
+    public DrawFrame() {
+        startProgram();
+    }
+
+    private void startProgram() {
+
         bExit = new JButton("Exit");
-        bExit.setBounds(1,125,200,25);
+        bSort = new JButton("Sort");
+        myFrame = new JFrame();
+        numberInformation = new JLabel("Enter value to sort");
+        sortedNumbers = new JLabel();
+        setTitle("sortowanie");
+        setSize(400, 250);
+        imputNumbersTextArea = new JTextField("");
+        setLayout(null);
+        bExit.setBounds(2, 150, 100, 20);
+        bSort.setBounds(250, 150, 100, 20);
+        imputNumbersTextArea.setBounds(122, 17, 150, 20);
+        numberInformation.setBounds(2, 12, 150, 30);
+        sortedNumbers.setBounds(2, 50, 150, 30);
         add(bExit);
+        add(bSort);
+        add(imputNumbersTextArea);
+        add(numberInformation);
+        add(sortedNumbers);
+        setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        bPrintInfo.addActionListener(this);
         bExit.addActionListener(this);
-    }
+        bSort.addActionListener(this);
 
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        Object sourse = e.getSource();
-        if (sourse == bPrintInfo){
-                System.out.println("Nacisk");
-    } else if (sourse == bExit) {
-            System.out.println("Koniec programu Wyjście");
-           dispose();
+        Object source = e.getSource();
+        if (source == bExit) {
+            dispose();
+        } else if (source == bSort) {
+            inputedNumbersTextField = imputNumbersTextArea.getText();
+            sortedNumbers.setText(inputedNumbersTextField);
+           table = new String [inputedNumbersTextField.length()];
+           table=inputedNumbersTextField.split(", ");
+            for (String table:table) {
+                int number = Integer.parseInt(table.trim());
+                numbers.add(number);
+            }
+        }
+//
+//            Collections.sort(numbers);
+            System.out.println(numbers);
 
         }
-
     }
-}
+
+
+
+
+
+
+
+
+
---
+++ Sortowanie_UI/src/Numbers.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Numbers {
+DrawFrame drawFrame;
+//ArrayList<DrawFrame> frame = new ArrayList<>();
+public void chaneValue (){
+
+    System.out.println( drawFrame.getInputedNumbersTextField());
+
+    }
+    }
+
+
